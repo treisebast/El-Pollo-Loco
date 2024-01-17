4db58441745db1d40825ef67086a1685dc9cb969
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-01-17 16:14:09 +0100

    add sound to character

--- js/models/character.class.js
+++ js/models/character.class.js
@@ -11,7 +11,7 @@ class Character extends MovableObject {
         "img/2_character_pepe/2_walk/W-26.png"
     ];
     world;
-
+    walking_sound = new Audio('audio/running.mp3');
 
     constructor() {
         super().loadImage("img/2_character_pepe/2_walk/W-21.png");
@@ -22,14 +22,18 @@ class Character extends MovableObject {
 
 
     animate() {
-        setInterval(() =>{
+        setInterval(() =>{   // Character walking left or rigth
+            this.walking_sound.pause();
+
             if (this.world.keyboard.RIGHT && this.x < this.world.level.level_end_x) {
                 this.x += this.speed;
                 this.otherDirection = false;
+                this.walking_sound.play();
             }
             if (this.world.keyboard.LEFT && this.x > 0) {
                 this.x -= this.speed;
                 this.otherDirection = true;
+                this.walking_sound.play();
             }
             this.world.camera_x = -this.x + 100;
         }, 25); //Speed character
