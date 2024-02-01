ca7714ae7c61cf89484e997f2b39da0f42b81b03
Sebastian Treittinger <sebastian.treittinger@gmail.com>
2024-02-01 05:59:09 +0100

    add_touch_buttons

--- js/game.js
+++ js/game.js
@@ -2,10 +2,10 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-function startGame(){
-    document.getElementById('startScreen').style.display = 'none';
-    document.getElementById('canvas').style.display = 'flex';
-    document.getElementById('headerPenel').style.justifyContent = 'center';
+function startGame() {
+    document.getElementById("startScreen").style.display = "none";
+    document.getElementById("canvas").style.display = "flex";
+    document.getElementById("headerPenel").style.justifyContent = "center";
     init();
 }
 
@@ -13,80 +13,23 @@ function startGame(){
 function init() {
     canvas = document.getElementById("canvas");
     world = new World(canvas, keyboard);
-    
-    console.log(world.level.enemies);
-    console.log('My charakter is', world.character);
 
+    console.log(world.level.enemies);
+    console.log("My charakter is", world.character);
 }
 
 
 function handleGameEnd(winOrLose) {
     console.log("Spiel beendet!");
 
-    let endScreen = document.getElementById('endScreen');
-    endScreen.style.display = 'flex'; 
+    let endScreen = document.getElementById("endScreen");
+    endScreen.style.display = "flex";
 
-    let imageEndScreen = document.getElementById('imageEndScreen');
-    if (winOrLose == 'youWin') {
-        imageEndScreen.src = 'img/9_intro_outro_screens/game_over/game over!.png';  
+    let imageEndScreen = document.getElementById("imageEndScreen");
+    if (winOrLose == "youWin") {
+        imageEndScreen.src = "img/9_intro_outro_screens/game_over/game over!.png";
     } else {
-        imageEndScreen.src = 'img/9_intro_outro_screens/game_over/oh no you lost!.png';
+        imageEndScreen.src = "img/9_intro_outro_screens/game_over/oh no you lost!.png";
     }
 }
 
-
-
-document.addEventListener('keydown', (e) => {
-
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = true;
-    }
-
-    if (e.keyCode == 37) {
-        keyboard.LEFT = true;
-    }
-
-    if (e.keyCode == 38) {
-        keyboard.UP = true;
-    }
-
-    if (e.keyCode == 40) {
-        keyboard.DOWN = true;
-    }
-
-    if (e.keyCode == 32) {
-        keyboard.SPACE = true;
-    }
-
-    if (e.keyCode == 68 ) {
-        keyboard.D = true;
-    }
-});
-
-
-document.addEventListener('keyup', (e) => {
-    if (e.keyCode == 39) {
-        keyboard.RIGHT = false;
-    }
-
-    if (e.keyCode == 37) {
-        keyboard.LEFT = false;
-    }
-
-    if (e.keyCode == 38) {
-        keyboard.UP = false;
-    }
-
-    if (e.keyCode == 40) {
-        keyboard.DOWN = false;
-    }
-
-    if (e.keyCode == 32) {
-        keyboard.SPACE = false;
-    }
-
-    if (e.keyCode == 68 ) {
-        keyboard.D = false;
-    }
-
-});
--- js/models/keyboard.class.js
+++ js/models/keyboard.class.js
@@ -1,4 +1,4 @@
-class Keyboard{
+class Keyboard {
     LEFT = false;
     RIGHT = false;
     UP = false;
@@ -6,8 +6,104 @@ class Keyboard{
     SPACE = false;
     D = false;
 
+    constructor() {
+        this.bindBtnsTouchEvents();
+        this.bindKeybuttonsEvents();
+    }
 
+    bindKeybuttonsEvents() {
+        document.addEventListener("keydown", (e) => {
+            if (e.keyCode == 39) {
+                this.RIGHT = true;
+            }
 
+            if (e.keyCode == 37) {
+                this.LEFT = true;
+            }
 
-    
+            if (e.keyCode == 38) {
+                this.UP = true;
+            }
+
+            if (e.keyCode == 40) {
+                this.DOWN = true;
+            }
+
+            if (e.keyCode == 32) {
+                this.SPACE = true;
+            }
+
+            if (e.keyCode == 68) {
+                this.D = true;
+            }
+        });
+
+        document.addEventListener("keyup", (e) => {
+            if (e.keyCode == 39) {
+                this.RIGHT = false;
+            }
+
+            if (e.keyCode == 37) {
+                this.LEFT = false;
+            }
+
+            if (e.keyCode == 38) {
+                this.UP = false;
+            }
+
+            if (e.keyCode == 40) {
+                this.DOWN = false;
+            }
+
+            if (e.keyCode == 32) {
+                this.SPACE = false;
+            }
+
+            if (e.keyCode == 68) {
+                this.D = false;
+            }
+        });
+    }
+
+    bindBtnsTouchEvents() {
+        document.getElementById("btnLeft").addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            this.LEFT = true;
+        });
+
+        document.getElementById("btnLeft").addEventListener("touchend", (e) => {
+            e.preventDefault();
+            this.LEFT = false;
+        });
+
+        document.getElementById("btnRight").addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            this.RIGHT = true;
+        });
+
+        document.getElementById("btnRight").addEventListener("touchend", (e) => {
+            e.preventDefault();
+            this.RIGHT = false;
+        });
+
+        document.getElementById("btnThrow").addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            this.D = true;
+        });
+
+        document.getElementById("btnThrow").addEventListener("touchend", (e) => {
+            e.preventDefault();
+            this.D = false;
+        });
+
+        document.getElementById("btnJump").addEventListener("touchstart", (e) => {
+            e.preventDefault();
+            this.SPACE = true;
+        });
+
+        document.getElementById("btnJump").addEventListener("touchend", (e) => {
+            e.preventDefault();
+            this.SPACE = false;
+        });
+    }
 }
